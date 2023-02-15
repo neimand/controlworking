fc4e2c879410c56794035032a1e883735174a86b
Darya <kopteleva@inbox.ru>
2023-02-15 15:11:58 +0300

    добавлены методы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
 ﻿// Задание: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
 
+//-------------Методы--------------
+string[] NewArray(string[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] resultArray = new string[count];
+    count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            resultArray[count] = array[i];
+            count++;
+        }
+    }
+    return resultArray;
+}
+
+void PrintArray(string[] array)
+{
+    Console.WriteLine($"[{String.Join(",", array)}]");
+}
+
